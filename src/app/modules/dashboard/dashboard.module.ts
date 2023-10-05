7bc18d6897c42272f873e7569e225143b2c6d17b
[200~MrLexJR <jonarsa_13@hotmail.com>
2023-10-05 10:28:57 -0500

    Creacion de nav y menu

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -3,17 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './pages/dashboard.component';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
-  declarations: [
-    DashboardComponent,
-    HomeComponent
-  ],
-  imports: [
-    CommonModule,
-    RouterModule
-  ]
+  declarations: [DashboardComponent, HomeComponent],
+  imports: [CommonModule, RouterModule, SharedModule],
 })
-export class DashboardModule { }
+export class DashboardModule {}
